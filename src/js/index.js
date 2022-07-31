a6156b4bc8c4e933deaa023c79b7edf68dba133d
Elena Zayceva <alenita.zayceva@gmail.com>
2022-08-01 00:50:55 +0300

    feat: create my portfolio

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,30 @@
 const hamburger = document.querySelector('.hamburger');
 const menu = document.querySelector('.menu');
 const closeElem = document.querySelector('.menu__close');
+const overlay = document.querySelector('.menu__overlay');
+const menuList = document.querySelector('.menu__list');
+const menuLinks = document.querySelectorAll('.menu__link');
 
 hamburger.addEventListener('click', () => {
    menu.classList.add('active');
+   document.querySelector('html').style.overflow = 'hidden';
 });
 
 closeElem.addEventListener('click', () => {
    menu.classList.remove('active');
+   document.querySelector('html').style.overflow = '';
+});
+
+overlay.addEventListener('click', () => {
+   menu.classList.remove('active');
+   document.querySelector('html').style.overflow = '';
+});
+
+menuLinks.forEach(function(link) {
+   link.addEventListener('click', (e) => {
+      menu.classList.remove('active');
+      document.querySelector('html').style.overflow = '';
+});
 });
 
 const counters = document.querySelectorAll('.skills__ratings-counter');
